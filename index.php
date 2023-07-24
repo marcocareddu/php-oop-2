a7c8b6e5d3c34af0ddd3b618cdef542abbc06b95
marcocareddu <saldumaccu@tuta.io>
2023-07-24 14:53:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Include Head -->
+<?php include __DIR__ . './includes/head.inc'; ?>
+
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="mt-3">
+
+            <h1>Boolshop</h1>
+            <div class="pt-3">
+
+                <!-- Foreach Here -->
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
